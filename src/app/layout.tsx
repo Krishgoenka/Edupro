d0b1aa6179d85d408cc78d6f6e152df2dce71fe4
Krish Goenka <goenkakrish02@gmail.com>
2025-07-07 07:02:17 +0000

    meta tags for google search console for verification take this and u kno

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,6 +46,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <head>
+        <meta name="google-site-verification" content="1b87uVK69rxeLVBV6Cq-6-IKjw_ejsAa-s3TKrqVRpo" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Space+Grotesk:wght@500;700&display=swap" rel="stylesheet"></link>
