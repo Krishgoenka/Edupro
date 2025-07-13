398c7c6420d1f7929425b44db3ba3b094a5a5fd1
Krish Goenka <goenkakrish02@gmail.com>
2025-07-13 14:35:52 +0000

    The NextJS app server is having trouble starting. Please identify what c

---
+++ src/functions/index.ts
@@ -0,0 +1,8 @@
+
+/**
+ * @fileOverview This file exports the Cloud Functions for the application.
+ * It serves as the entry point for Firebase Functions deployments.
+ */
+
+// Export the functions from the specific function files.
+export { setAdminRole } from './lib';
